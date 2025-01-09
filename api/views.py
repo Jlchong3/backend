f22c111888d69ef08cb407c6630e2d4fa01f13da
Jose Chong <127264125+Jlchong3@users.noreply.github.com>
2025-01-09 00:12:44 -0500

    done

--- api/views.py
+++ api/views.py
@@ -35,3 +35,49 @@ class LandingAPI(APIView):
 
         return Response({'id': new_resource.key}, status=status.HTTP_201_CREATED)
 
+
+class LandingAPIDetail(APIView):
+
+    name = 'Landing Detail API'
+
+    collection_name = 'data'
+
+    def get(self, request, pk):
+        ref = db.reference(f'{self.collection_name}')
+        documento = dict(ref.get()).get(pk)
+
+        if documento:
+            return Response(documento, status=status.HTTP_200_OK)
+        else:
+            return Response('No se encuentra el documento', status=status.HTTP_404_NOT_FOUND)
+
+
+    def put(self, request, pk):
+        ref = db.reference(f'{self.collection_name}')
+
+        documento = dict(ref.get()).get(pk)
+
+        if not documento:
+            return Response('No se encuentra el documento', status=status.HTTP_404_NOT_FOUND)
+
+        if 'email' not in request.data:
+            return Response('La petición no tienen los campos requeridos', status=status.HTTP_400_BAD_REQUEST)
+
+        for k, v in request.data.items():
+            if k in documento:
+                documento[k] = v
+
+        ref.child(pk).set(documento)
+
+        return Response('Item actualizado', status=status.HTTP_200_OK)
+
+    def delete(self, request, pk):
+
+        ref = db.reference(f'{self.collection_name}')
+
+        documento = dict(ref.get()).get(pk)
+        if not documento:
+            return Response('No se encuentra el documento', status=status.HTTP_404_NOT_FOUND)
+
+        ref.child(pk).delete()
+        return Response('Se ha eliminado el documento', status=status.HTTP_200_OK)
